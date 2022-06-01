36e38ba6c2ca2c4a30f02805c3d4843cfba96821
Yuri <yuri.araujo-ext@tuibrasil.com>
2022-06-01 11:24:01 -0400

    feat: add inn module

--- src/store/modules/inn.js
+++ src/store/modules/inn.js
@@ -1,46 +1,19 @@
-const state = () => ({})
+const state = () => ({
+  activeInn: null,
+})
 
-const getters = {
-  // cartProducts: (state, getters, rootState) => {
-  //   return state.items.map(({ id, quantity }) => {
-  //     const product = rootState.products.all.find(
-  //       (product) => product.id === id
-  //     )
-  //     return {
-  //       id: product.id,
-  //       title: product.title,
-  //       price: product.price,
-  //       quantity,
-  //     }
-  //   })
-  // },
-}
+const getters = {}
 
 const actions = {
-  // checkout({ commit, state }, products) {
-  //   const savedCartItems = [...state.items]
-  //   commit('setCheckoutStatus', null)
-  //   // empty cart
-  //   commit('setCartItems', { items: [] })
-  //   shop.buyProducts(
-  //     products,
-  //     () => commit('setCheckoutStatus', 'successful'),
-  //     () => {
-  //       commit('setCheckoutStatus', 'failed')
-  //       // rollback to the cart saved before sending the request
-  //       commit('setCartItems', { items: savedCartItems })
-  //     }
-  //   )
-  // },
+  selectActiveInn({ commit }, inn) {
+    commit('setActiveInn', inn || null)
+  },
 }
 
 const mutations = {
-  // pushProductToCart(state, { id }) {
-  //   state.items.push({
-  //     id,
-  //     quantity: 1,
-  //   })
-  // },
+  setActiveInn(state, inn) {
+    state.activeInn = inn
+  },
 }
 
 export default {
